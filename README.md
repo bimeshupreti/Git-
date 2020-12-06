201361fb23620574b4b68664e7fe9a955e171490
bimeshupreti <bimesh.upreti@gmail.com>
2020-12-06 19:12:44 +0200

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Git-
-its for git practice
--- test2.py
+++
@@ -1,6 +0,0 @@
-myList = [5, 10, 2, 12, 3, 9, 3, 4]
-sum = 0
-for i in range(len(myList)): # len(myList) = 7
-    sum +=myList[i] # i is from 0 to 4
-
-print (sum) 
